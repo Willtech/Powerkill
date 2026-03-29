0fbeba6c0fb86e9be4af17fea6382b99da54b5fc
Damian Williamson <willtech@live.com.au>
2026-03-29 20:47:47 +1100

    powerkill.spec Initial Commit.
    packaging/powerkill.spec Initial Commit.
    
    Signed-off-by: Damian Williamson <willtech@live.com.au>

--- cpueater.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-## CPU Eater
-# Tries to take up a lot of CPU time
-# cpueater.py script
-# Source Code produced by Willtech 2023
-# v1.0 hand coded by HRjJ
-x = 1
-while True:
- x = not x
